b057a00da1cfeaffd3735c53d596f570b9f643c6
Sarah-Tereza <sarahteresadasilva@gmail.com>
2026-05-21 22:30:51 -0300

    Update exercicio05.py
    This change is because i don't remember finishing the lesson before sending it to the repository

--- atividade 07/exercicio05.py
+++ atividade 07/exercicio05.py
@@ -3,13 +3,20 @@
 #ímpares por 0 e ao final, exibir a nova matriz.
 
 matriz=[]
+new_matriz=[]
 for a in range(4):
     lista=[]
     for b in range(4):
-        num=float(input('digite um número:'))
-        if num%2==0:
-            lista.append(1)
-        else:
-            lista.append(0)
-    matriz.append(lista)
-print(f'essa é a nova matriz:{matriz}')
+        linha.append(float(input('digite um número:')))
+    matriz.append(linha) 
+print(f'essa é a matriz original{matriz}')
+
+for a in range(4):
+    new_linha=[]
+    for b in range(4):
+        if matriz[a][b]%2==0:
+            new_lista.append(1)
+        elif matriz[a][b] % 2 !=0:
+            new_linha.append(0)
+    matriz.append(new_linha)
+print(f'essa é a nova matriz:{new_matriz}')

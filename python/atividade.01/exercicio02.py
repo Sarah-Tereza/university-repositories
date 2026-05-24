a7c7218dbef5f21a31acd60d24a646465eb31f41
Sarah-Tereza <sarahteresadasilva@gmail.com>
2026-05-24 18:16:53 -0300

    Create exercicio02.py

---
+++ python/atividade.01/exercicio02.py
@@ -0,0 +1,7 @@
+lado = int(input("qual a metragem da largura do comodo?"))
+comp = int(input('qual a metragem do comprimento do comodo?'))
+area = lado * comp
+luz = 18
+what = area * luz
+print(area)
+print(what)

8a7902969ff56600cd16136b3fabd9d2b312e1d4
Sarah-Tereza <sarahteresadasilva@gmail.com>
2026-05-24 18:18:47 -0300

    Create exercicio03.py

---
+++ python/atividade.01/exercicio03.py
@@ -0,0 +1,5 @@
+valor = int(input('valor da hora:'))
+hora = int(input('horas trabalhadas:'))
+salario = valor * hora
+final = salario + (salario * 0.12)
+print('salario final com acrescimo', final, 'reais')
